c29f24590540cdeba8ee7b34073ef765fb0f352c
rlatnfh99 <rlatnfh2004@naver.com>
2017-11-06 16:50:11 +0900

    add Centos

---
+++ phpjs/7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>JavaScript</h1>
+	<script>
+		password = prompt("Password");
+		document.write(password);
+		if(password==1111){
+			document.write("Hello master!");
+		}else{
+			document.write("who are you?");
+		}
+	</script>
+	<h1>php</h1>
+	<?php
+		
+	?>
+	
+</body>
+</html>
